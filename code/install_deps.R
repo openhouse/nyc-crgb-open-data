255c6ea9ce2ca5f82ced0c0702bed0fb0ace9b6b
James "Jamie" Burkart @openhouse <jamie@ohai.us>
2025-11-29 12:14:50 -0500

    successful end-to-end scaffolding run

--- code/install_deps.R
+++ code/install_deps.R
@@ -1,17 +1,18 @@
 # Minimal dependency installer for the nyc-crgb-open-data project.
 # Run from the project root: source("code/install_deps.R")
 
-pkgs <- c(
-  "here",
-  "tidyverse",
-  "sf",
-  "yaml",
-  "targets",
-  "gt",
-  "glue",
-  "janitor"
-  # Optional theming packages: "councilverse" / "councildown"
-)
+  pkgs <- `c(
+    "here",
+    "tidyverse",
+    "sf",
+    "yaml",
+    "targets",
+    "gt",
+    "glue",
+    "janitor",
+    "councilverse",
+    "councildown"
+  )`
 
 to_install <- setdiff(pkgs, rownames(installed.packages()))
 if (length(to_install)) {
